1ab012023d87288f6dbdfd3638e7b23468d2c30f
Davide Giliberti <gilo.davide@hotmail.it>
2024-10-17 01:02:37 +0200

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,7 +3,10 @@ import {
   Button,
   Card,
   Col,
+  FloatingLabel,
+  Form,
   ListGroup,
+  Modal,
   Placeholder,
   Row,
 } from "react-bootstrap";
@@ -11,6 +14,43 @@ import {
 const Admin = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    titolo: "",
+    descrizione: "",
+    annoDiPubblicazione: 0,
+    piattaforma: "",
+    genere: "",
+    totaleOreDiGioco: 0,
+    boxArt: undefined,
+  });
+  const [show, setShow] = useState(false);
+  const [showPatch, setShowPatch] = useState(false);
+  const [showPut, setShowPut] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleClosePatch = () => setShowPatch(false);
+  const handleClosePut = () => setShowPut(false);
+  const handleCloseDelete = () => setShowDelete(false);
+  const handleShow = () => setShow(true);
+  const handleShowPatch = () => setShowPatch(true);
+  const handleShowPut = () => setShowPut(true);
+  const handleShowDelete = () => setShowDelete(true);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const resetForm = () => {
+    setFormData({
+      titolo: "",
+      descrizione: "",
+      annoDiPubblicazione: 0,
+      piattaforma: "",
+      genere: "",
+      totaleOreDiGioco: 0,
+      boxArt: undefined,
+    });
+  };
+
+  //chiamata GET
 
   const fetchData = async () => {
     try {
@@ -35,28 +75,392 @@ const Admin = () => {
     }
   };
 
+  //chiamata POST
+
+  const postData = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:3001/videogiochi";
+      const token = localStorage.getItem("authToken");
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        fetchData();
+        resetForm();
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //chiamata PATCH
+
+  const patchData = async (file) => {
+    try {
+      const url = `http://localhost:3001/videogiochi/${localStorage.getItem(
+        "videogiocoId"
+      )}/boxArt`;
+      const token = localStorage.getItem("authToken");
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        fetchData();
+        resetForm();
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //chiamata PUT
+
+  const putData = async (updatedData) => {
+    try {
+      const url = `http://localhost:3001/videogiochi/${localStorage.getItem(
+        "videogiocoId"
+      )}`;
+      const token = localStorage.getItem("authToken");
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      });
+
+      if (response.ok) {
+        fetchData();
+        resetForm();
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //chiamata DELETE
+
+  const deleteData = async (id) => {
+    try {
+      const url = `http://localhost:3001/videogiochi/${localStorage.getItem(
+        "videogiocoId"
+      )}`;
+      const token = localStorage.getItem("authToken");
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        fetchData();
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    patchData(file);
+  };
+
+  const getId = (id) => {
+    localStorage.setItem("videogiocoId", id);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <>
+      {/* modale creazione videogioco */}
+      <Modal show={show} onHide={handleClose} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Inserisci videogioco</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={postData}>
+          <Row>
+            <Col xl={12}>
+              <FloatingLabel controlId="titolo" label="Titolo">
+                <Form.Control
+                  type="text"
+                  name="titolo"
+                  placeholder="Titolo"
+                  value={formData.titolo}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="descrizione" label="Descrizione">
+                <Form.Control
+                  type="text"
+                  name="descrizione"
+                  placeholder="Descrizione"
+                  value={formData.descrizione}
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel
+                controlId="annoDiPubblicazione"
+                label="Anno di pubblicazione"
+              >
+                <Form.Control
+                  type="number"
+                  name="annoDiPubblicazione"
+                  placeholder="Anno di pubblicazione"
+                  value={formData.annoDiPubblicazione}
+                  onChange={handleChange}
+                  required
+                  min="1970"
+                  max="2100"
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="piattaforma" label="Piattaforma">
+                <Form.Control
+                  type="text"
+                  name="piattaforma"
+                  placeholder="Piattaforma"
+                  value={formData.piattaforma}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="genere" label="Genere">
+                <Form.Control
+                  type="text"
+                  name="genere"
+                  placeholder="Genere"
+                  value={formData.genere}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Modal.Footer>
+            <Button
+              variant="success"
+              type="submit"
+              className="m-3"
+              onClick={handleClose}
+            >
+              Conferma
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+
+      {/* modale della box art */}
+
+      <Modal show={showPatch} onHide={handleClosePatch} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Inserisci box art</Modal.Title>
+        </Modal.Header>
+        <Form>
+          <Form.Control
+            type="file"
+            name="boxArt"
+            placeholder="box art"
+            value={formData.boxArt}
+            onChange={handleFileChange}
+          />
+          <Modal.Footer>
+            <Button
+              variant="success"
+              type="button"
+              className="m-3"
+              onClick={(e) => {
+                e.preventDefault();
+                handleClosePatch();
+              }}
+            >
+              Conferma
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+
+      {/* modale modifica dati */}
+
+      <Modal show={showPut} onHide={handleClosePut} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Modifica dati</Modal.Title>
+        </Modal.Header>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const updatedData = {
+              titolo: formData.titolo,
+              descrizione: formData.descrizione,
+              annoDiPubblicazione: formData.annoDiPubblicazione,
+              piattaforma: formData.piattaforma,
+              genere: formData.genere,
+              totaleOreDiGioco: formData.totaleOreDiGioco,
+            };
+            putData(updatedData);
+          }}
+        >
+          <Row>
+            <Col xl={12}>
+              <FloatingLabel controlId="titolo" label="Titolo">
+                <Form.Control
+                  type="text"
+                  name="titolo"
+                  placeholder="Titolo"
+                  value={formData.titolo}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="descrizione" label="Descrizione">
+                <Form.Control
+                  type="text"
+                  name="descrizione"
+                  placeholder="Descrizione"
+                  value={formData.descrizione}
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel
+                controlId="annoDiPubblicazione"
+                label="Anno di pubblicazione"
+              >
+                <Form.Control
+                  type="number"
+                  name="annoDiPubblicazione"
+                  placeholder="Anno di pubblicazione"
+                  value={formData.annoDiPubblicazione}
+                  onChange={handleChange}
+                  required
+                  min="1970"
+                  max="2100"
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="piattaforma" label="Piattaforma">
+                <Form.Control
+                  type="text"
+                  name="piattaforma"
+                  placeholder="Piattaforma"
+                  value={formData.piattaforma}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+            <Col xl={12}>
+              <FloatingLabel controlId="genere" label="Genere">
+                <Form.Control
+                  type="text"
+                  name="genere"
+                  placeholder="Genere"
+                  value={formData.genere}
+                  onChange={handleChange}
+                  required
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Modal.Footer>
+            <Button
+              variant="success"
+              type="submit"
+              className="m-3"
+              onClick={handleClosePut}
+            >
+              Conferma
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+
+      {/* modale eleminazione videogioco */}
+
+      <Modal show={showDelete} onHide={handleCloseDelete} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Sei sicuro di voler eleminare questo videogioco?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button
+            variant="danger"
+            onClick={() => {
+              deleteData();
+              handleCloseDelete();
+            }}
+          >
+            Elimina
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* inizio pagina catalogo */}
+
       <h1 className="text-white text-center my-3">Gestione catalogo</h1>
       <div className="d-flex justify-content-center gap-3">
-        <Button variant="primary">Aggiungi titolo</Button>
-        <Button variant="danger">Elimina titolo</Button>
+        <Button variant="primary" onClick={handleShow} className="mb-3">
+          Aggiungi titolo
+        </Button>
       </div>
       <Row>
         {data && data.content && data.content.length > 0 ? (
           data.content.map((videogioco) => (
-            <Col key={videogioco.id} md={6}>
+            <Col key={videogioco.id} md={6} className="mb-5">
               <Card
                 bg="dark"
                 text="white"
-                style={{ width: "18rem" }}
-                border="dark"
+                style={{ width: "27rem" }}
+                border="light"
               >
-                <Card.Img variant="top" src={videogioco.boxArt} />
+                <Card.Img
+                  variant="top"
+                  src={videogioco.boxArt}
+                  className="img-fluid"
+                />
                 <Card.Body>
                   <Card.Title className="text-center">
                     {videogioco.titolo}
@@ -75,9 +479,35 @@ const Admin = () => {
                       Genere: {videogioco.genere}
                     </ListGroup.Item>
                   </ListGroup>
-                  <Button variant="primary" className="mt-3">
-                    Modifica
-                  </Button>
+                  <div className="d-flex mt-3 gap-3 justify-content-center">
+                    <Button
+                      variant="primary"
+                      onClick={() => {
+                        getId(videogioco.id);
+                        handleShowPatch();
+                      }}
+                    >
+                      Aggiungi box art
+                    </Button>
+                    <Button
+                      variant="warning"
+                      onClick={() => {
+                        getId(videogioco.id);
+                        handleShowPut();
+                      }}
+                    >
+                      Modifica
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        getId(videogioco.id);
+                        handleShowDelete();
+                      }}
+                    >
+                      Elimina titolo
+                    </Button>
+                  </div>
                 </Card.Body>
               </Card>
             </Col>
