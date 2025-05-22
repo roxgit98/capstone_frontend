5ec6031eef5f91756088382ec2521c6e709638d0
Davide Giliberti <gilo.davide@hotmail.it>
2025-05-23 00:53:25 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
     try {
       const response = await fetch(
-        "optimistic-dorris-roxgit98-b4431215.koyeb.app/authorization/login",
+        "http://optimistic-dorris-roxgit98-b4431215.koyeb.app/authorization/login",
         {
           method: "POST",
           headers: {
